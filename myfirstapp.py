7ad510bb2808620ae2eb5d221ad0426567712378
dgohks <112739064+dgohks@users.noreply.github.com>
2022-09-03 10:41:47 +0800

    Update myfirstapp.py

--- myfirstapp.py
+++ myfirstapp.py
@@ -11,7 +11,7 @@ readme = st.checkbox("readme first")
 if readme:
 
     st.write("""
-        This is a web app demo using [streamlit](https://streamlit.io/) library. It is hosted on [heroku](https://www.heroku.com/). You may get the codes via [github](https://github.com/richieyuyongpoh/myfirstapp)
+        This is a web app demo using [streamlit](https://streamlit.io/) library. It is hosted on [Streamlit Cloud](https://www.streamlit.io/). You may get the codes via [github](https://github.com/dgohks/myfirstwebapp)
         """)
 
     st.write ("For more info, please contact:")
